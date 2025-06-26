064db1acd4f08332fe006492ccae1190270de5f9
Intakhab Alam <77919353+Intakhab-Alam@users.noreply.github.com>
2025-06-26 15:12:39 +0530

    adding repository interface and classes

---
+++ ProductCatalog/ProductCatalog.API/Exceptions/NotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace ProductCatalog.API.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message) { }
+    }
+}
---
+++ ProductCatalog/ProductCatalog.API/Repositories/ICustomerRepository.cs
@@ -0,0 +1,8 @@
+﻿using ProductCatalog.API.Models;
+namespace ProductCatalog.API.Repositories
+{
+    public interface ICustomerRepository
+    {
+        Task<Customer?> GetByIdAsync(int id);
+    }
+}
